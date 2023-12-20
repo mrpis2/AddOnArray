a71227d498b67aa2139ba03926c1f92cf890593f
mrpis2 <warhammerprogamer@gmail.com>
2023-12-20 22:31:11 +0700

    AddOnArray

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chieu dai mang");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[n+1];
+        Console.WriteLine("Nhap cac gia tri cua mang");
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Gia tri cua mang la: ");
+        for(int i = 0;i < n; i++)
+        {
+            Console.Write(array[i] + "  ");
+        }
+        Console.WriteLine("Nhap gia tri can them: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap vi tri can them: ");
+        int p = int.Parse(Console.ReadLine());
+        for (int i = n; i >=p; i--)
+        {
+            array[i] = array[i-1];
+        }
+        array[p - 1] = x;
+        Console.WriteLine("Mang moi la: ");
+        for (int i = 0; i<n; i++)
+        {
+            Console.Write(array[i] + "  ");
+        }
+    }
+}
